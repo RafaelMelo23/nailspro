ad027b50b7f0e54c1abb3733a3c6700bab3c4fff
RafaelMelo23 <eooraff@gmail.com>
2025-12-14 16:06:47 -0300

    feat(exception handling): created the classes for ex handling

---
+++ src/main/java/com/rafael/nailspro/webapp/service/infra/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.rafael.nailspro.webapp.service.infra.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
---
+++ src/main/java/com/rafael/nailspro/webapp/service/infra/exception/StandardError.java
@@ -0,0 +1,16 @@
+package com.rafael.nailspro.webapp.service.infra.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class StandardError implements Serializable {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+}
