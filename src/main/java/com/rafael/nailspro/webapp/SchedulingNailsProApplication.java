4ed7db2f756fc709fdcb552dd511cc9f4d6a849b
rafadev <eooraff@gmail.com>
2026-02-24 11:37:24 -0300

    (annotation): adds method security

--- src/main/java/com/rafael/nailspro/webapp/SchedulingNailsProApplication.java
+++ src/main/java/com/rafael/nailspro/webapp/SchedulingNailsProApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+@EnableMethodSecurity
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
