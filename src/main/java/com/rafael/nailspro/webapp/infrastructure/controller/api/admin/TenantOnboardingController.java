a917f67c675c40a3e3a4740d12b70cddd70b3845
rafadev <eooraff@gmail.com>
2026-03-02 13:53:14 -0300

    feat(super admin): adds super admin role

--- src/main/java/com/rafael/nailspro/webapp/infrastructure/controller/api/admin/TenantOnboardingController.java
+++ src/main/java/com/rafael/nailspro/webapp/infrastructure/controller/api/admin/TenantOnboardingController.java
@@ -8,23 +8,19 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/internal/onboard")
+@PreAuthorize("hasRole('SUPER_ADMIN')")
 public class TenantOnboardingController {
-
-    @Value("${internal.api.key}")
-    private String key;
 
     private final OnboardingService onboardingService;
 
     @PostMapping
-    public ResponseEntity<OnboardingResultDTO> onboardNewClient(@Valid @RequestBody OnboardingRequestDTO dto,
-                                                                @RequestHeader ("X-API-Key") String apiKey) {
-
-        if (!key.equals(apiKey)) throw new BusinessException("Invalid API Key");
+    public ResponseEntity<OnboardingResultDTO> onboardNewClient(@Valid @RequestBody OnboardingRequestDTO dto) {
 
         return ResponseEntity.ok(onboardingService.onboardOwner(dto));
     }
